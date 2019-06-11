ea7ecce1c9f8acc455a3538f824a54ab1bf2941b
norbertk <norbertk@lavaleriana.net>
2019-06-11 07:07:46 +0200

    added package.json

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir /home/pi/nano;
 cd /home/pi/nano;
+touch package.json;
 npm install avrgirl-arduino;
 wget -O smt172.hex https://raw.githubusercontent.com/nrbrt/smt172-nano/master/smt172.hex;
 wget -O programnano.js https://raw.githubusercontent.com/nrbrt/smt172-nano/master/programnano.js;
