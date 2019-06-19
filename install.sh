5d4eae140cde42123ac979f238eb6acf242fb26a
norbertk <norbertk@lavaleriana.net>
2019-06-19 13:10:08 +0200

    added /home/pi/nano directory clean up

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ npm install avrgirl-arduino;
 wget -O smt172.hex https://raw.githubusercontent.com/nrbrt/smt172-nano/master/smt172.hex;
 wget -O programnano.js https://raw.githubusercontent.com/nrbrt/smt172-nano/master/programnano.js;
 node /home/pi/nano/programnano.js;
+cd /home/pi;
+rm -r /home/pi/nano;
